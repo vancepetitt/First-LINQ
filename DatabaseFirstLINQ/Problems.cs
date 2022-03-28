c811d36f60a3d908f513de2f9f37ee4049bc8920
Jacob-Petersen-1 <jacob.i.petersen52@gmail.com>
2022-03-28 13:00:47 -0600

    Answered Question number 3

--- DatabaseFirstLINQ/Problems.cs
+++ DatabaseFirstLINQ/Problems.cs
@@ -15,9 +15,9 @@ namespace DatabaseFirstLINQ
         }
         public void RunLINQQueries()
         {
-            ProblemOne();
+            //ProblemOne();
             //ProblemTwo();
-            //ProblemThree();
+            ProblemThree();
             //ProblemFour();
             //ProblemFive();
             //ProblemSix();
@@ -65,6 +65,17 @@ namespace DatabaseFirstLINQ
         {
             // Write a LINQ query that gets each product where the products price is greater than $150.
             // Then print the name and price of each product from the above query to the console.
+
+            var products = _context.Products;
+            foreach (Product product in products)
+            {
+                if (product.Price >= 150)
+                {
+                    Console.WriteLine(product.Name + ' ' + product.Price);
+                }
+            }
+            
+
 
         }
 
