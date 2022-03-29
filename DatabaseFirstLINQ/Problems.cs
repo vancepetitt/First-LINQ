897eec29639905248590dd8848147915cf41b810
Dylyn-Daughtry <98605975+Dylyn-Daughtry@users.noreply.github.com>
2022-03-29 11:12:54 -0400

    Completed Problem 18

--- DatabaseFirstLINQ/Problems.cs
+++ DatabaseFirstLINQ/Problems.cs
@@ -264,8 +264,10 @@ namespace DatabaseFirstLINQ
         private void ProblemEighteen()
         {
             // Delete the role relationship from the user who has the email "oda@gmail.com" using LINQ.
-
-        }
+            var userRole = _context.UserRoles.Where(ur => ur.User.Email == "oda@gmail.com").SingleOrDefault();
+            _context.UserRoles.Remove(userRole);
+            _context.SaveChanges();
+                }
 
         private void ProblemNineteen()
         {
@@ -281,9 +283,11 @@ namespace DatabaseFirstLINQ
 
         private void ProblemTwenty()
         {
-            // Delete the user with the email "oda@gmail.com" from the Users table using LINQ.
-
-        }
+        //    Delete the user with the email "oda@gmail.com" from the Users table using LINQ.
+        //   var user = _context.Users.Where(u => u.Email == "oda@gmail.com").SingleOrDefault();
+        //    _context.Users.Remove(user);
+        //    _context.SaveChanges();
+        //}
 
         // <><><><><><><><> BONUS PROBLEMS <><><><><><><><><>
 
