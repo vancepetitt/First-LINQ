00bba687b7b3dff866d8b40b866657c198201f8a
vance.petitt <vance.petitt@gmail.com>
2022-03-29 09:42:08 -0500

    Solved problem 16.

--- DatabaseFirstLINQ/Problems.cs
+++ DatabaseFirstLINQ/Problems.cs
@@ -28,9 +28,9 @@ namespace DatabaseFirstLINQ
             //ProblemEleven();
             //ProblemTwelve();
             //ProblemThirteen();
-            ProblemFourteen();
+            //ProblemFourteen();
             //ProblemFifteen();
-            //ProblemSixteen();
+            ProblemSixteen();
             //ProblemSeventeen();
             //ProblemEighteen();
             //ProblemNineteen();
@@ -236,6 +236,10 @@ namespace DatabaseFirstLINQ
         private void ProblemSixteen()
         {
             // Update the price of the product you created to something different using LINQ.
+            var product = _context.Products.Where(p => p.Name == "Nintendo Switch").SingleOrDefault();
+            product.Price = 249;
+            _context.Products.Update(product);
+            _context.SaveChanges();
 
         }
 
